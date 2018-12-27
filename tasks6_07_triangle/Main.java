aacfdb8682ed3c760ae2f20c2693a4ca21b2a69c
mzilatipak <45934585+mzilatipak@users.noreply.github.com>
2018-12-27 23:14:10 +0300

    Add files via upload

---
+++ tasks6_07_triangle/Main.java
@@ -0,0 +1,16 @@
+package by.htp.tasks6_07_triangle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Actions tr = new Actions();
+//		tr.getS();
+//		tr.getP();
+		
+		System.out.println("Ploshad treugolnika = " + tr.getS());
+		System.out.println("Perimetr treugolnika = " + tr.getP());
+
+	}
+
+}
